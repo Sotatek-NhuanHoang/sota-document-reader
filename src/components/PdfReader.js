b51c6e98fd6e4f23d0f940f937b440b91c26be97
Sotatek-NhuanHoang <nhuan.hoang@sotatek.com>
2019-06-12 00:47:46 +0700

    Complete pdf reader

--- src/components/PdfReader.js
+++ src/components/PdfReader.js
@@ -1,13 +1,50 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { pdfjs, Document, Page } from 'react-pdf';
+import _ from 'lodash';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 
 class PdfReader extends PureComponent {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pdfTotalPages: 0,
+        };
+    }
+
     render() {
+        const { url, options } = this.props;
+        const { pdfTotalPages } = this.state;
+        const w = window,
+            d = document,
+            e = d.documentElement,
+            g = d.getElementsByTagName('body')[0],
+            width = w.innerWidth || e.clientWidth || g.clientWidth;
+    
         return (
-            <div>
-                hoang duc nhuan
-            </div>
+            <Document
+                file={ url }
+                options={ options || {} }
+                onLoadSuccess={({ numPages }) => {
+                    this.setState({ pdfTotalPages: numPages });
+                }}
+            >
+                {_.map(new Array(pdfTotalPages), (item, index) => (
+                    <Fragment>
+                        <Page
+                            key={ index + 1 }
+                            pageNumber={ index + 1 }
+                            width={ width }
+                            loading=""
+                        />
+                        <hr />
+                    </Fragment>
+                ))}
+            </Document>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
+import './index.css';
+
+import '@babel/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+
 import PdfReader from './components/PdfReader';
 
 const rootEl = document.getElementById('root');
 
-const renderPdfReader = (url, options) => {
+window.renderPdfReader = function renderPdfReader(url, options) {
     ReactDOM.render(
         <AppContainer>
-            <PdfReader />
+            <PdfReader url={ url } options={ options } />
         </AppContainer>,
         rootEl
     );
 };
 
-renderPdfReader();
+window.renderSheetReader = function renderSheetReader(url, options) {
+    ReactDOM.render(
+        <AppContainer>
+            <PdfReader url={ url } options={ options } />
+        </AppContainer>,
+        rootEl
+    );
+};
+
+renderPdfReader('https://cors-anywhere.herokuapp.com/https://app.assetgenius.jp/api/assets/8', {
+    httpHeaders: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTE1LCJlbWFpbCI6InRlc3QtcG1AYXNzZXRnZW5pdXMuanAifQ.xBBSsGfToGRWviwLg-G-HD7PXVLFjmac1CWzox2nYvg`,
+        'X-AG-Mobile': 'true',
+        'Origin': 'app.assetgenius.jp',
+    },
+});
 
